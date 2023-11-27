ea3fdf52ecce831b20f0d3b4f5543e41f6c3e16f
olenanikitina1321 <o.nikitina@geniusee.com>
2023-11-27 21:03:40 +0100

    cypress homework

--- cypress/e2e/steps/steps.spec.js
+++ cypress/e2e/steps/steps.spec.js
@@ -1,41 +1,19 @@
-describe('Steps page', ()=>{
-  beforeEach(()=>{
-    cy.visit("/pages/layout/stepper")
-  })
-
+describe('Steps page', () =>{
 
   it('should show valid title on each step', () => {
-
+    cy.visit("/pages/layout/stepper")
     const containerSelector = 'nb-card-body nb-stepper.horizontal[orientation="horizontal"]'
 
-    cy.get(`${containerSelector} h3`).should('have.text', "Step content #1")
+    cy.get(`${containerSelector} h3`).should(`have.text`, "Step content #1")
     cy.get(`${containerSelector}`).contains('next').click()
 
-    cy.get(`${containerSelector} h3`).should('have.text', "Step content #2")
+    cy.get(`${containerSelector} h3`).should(`have.text`, "Step content #2")
     cy.get(`${containerSelector}`).contains('next').click()
 
-    cy.get(`${containerSelector} h3`).should('have.text', "Step content #3")
+    cy.get(`${containerSelector} h3`).should(`have.text`, "Step content #3")
     cy.get(`${containerSelector}`).contains('next').click()
 
-    cy.get(`${containerSelector} h3`).should('have.text', "Step content #4")
-    cy.get(`${containerSelector}`).contains('next').should('be.disabled')
-  });
-
-  it('should show valid title on each step (with aliases)', () => {
-    cy.get('nb-card-body nb-stepper.horizontal[orientation="horizontal"]').as('container')
-    cy.get('@container').find('h3').as('stepperTitle')
-    cy.get('@container').contains('next').as('nextButton')
-
-    cy.get('@stepperTitle').should('have.text', "Step content #1")
-    cy.get('@nextButton').click()
-
-    cy.get('@stepperTitle').should('have.text', "Step content #2")
-    cy.get('@nextButton').click()
-
-    cy.get('@stepperTitle').should('have.text', "Step content #3")
-    cy.get('@nextButton').click()
-
-    cy.get('@stepperTitle').should('have.text', "Step content #4")
-    cy.get('@nextButton').should('be.disabled')
-  });
+    cy.get(`${containerSelector} h3`).should(`have.text`, "Step content #4")
+    cy.get(`${containerSelector}`).contains('next').should(`be.disabled`)
+  })
 })
